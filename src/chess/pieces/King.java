a9efdd791f5afc2e3fd2016391ae5fbca9e42f05
Davi Araujo Cândido <daviaraujocandido@gmail.com>
2023-06-02 11:35:40 -0300

    implementing king moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,7 +18,45 @@ public class King extends ChessPiece {
     
     @Override
     public boolean[][] possibleMoves() {
-    	boolean [][] aux = new boolean[getBoard().getRows()][getBoard().getColumns()];
-    	return aux;
+        boolean [][] aux = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // above
+        p.setValues(position.getRow()-1, position.getColumn());
+        if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && getBoard().thereIsAPiece(p)) {
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left
+        p.setValues(position.getRow(), position.getColumn() -1);
+        if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && getBoard().thereIsAPiece(p)) {
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right
+        p.setValues(position.getRow(), position.getColumn() +1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && getBoard().thereIsAPiece(p)) {
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below
+        p.setValues(position.getRow()+1, position.getColumn());
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && getBoard().thereIsAPiece(p)) {
+            aux[p.getRow()][p.getColumn()] = true;
+        }
+        return aux;
     }
 }
